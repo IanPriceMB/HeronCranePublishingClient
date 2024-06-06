e70fc6e2e8cd5d8a648f4d245a9c78bd0fbf7b11
Ian Price <ianpriceve@gmail.com>
2024-06-06 14:44:59 -0500

    Basic set up.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+  return (
+    <>
+      <CssBaseline enableColorScheme />
+    </>
+  );
+}
+
+export default App;
