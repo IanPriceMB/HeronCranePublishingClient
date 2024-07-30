6a5330a8006166c9324be70d83830f8a06e664b3
Ian Price <ianpriceve@gmail.com>
2024-07-30 10:04:04 -0500

    react hook form

--- src/components/EmailForm/index.tsx
+++ src/components/EmailForm/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type Inputs = {
+  email: string
+  message: string
+}
 
 const EmailForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Email:</label>
         <br />
-        <input name="email" type='email' aria-label='eamil' />
+        <input type='email' aria-label='eamil' {...register("email", { required: true })} />
+        {errors.email && <span>This field is required</span>}
         <label>Message:</label>
         <br />
-        <input name='message' type='text' aria-label='message' />
+        <input type='text' aria-label='message' {...register("message", { required: true })} />
+        {errors.message && <span>This field is required</span>}
+        <input type='submit' />
       </form>
     </main>
   )
